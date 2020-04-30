dd6f8f75913d128c35e28220c38ac3a305bbb084
olgayue78 <63052143+olgayue78@users.noreply.github.com>
2020-04-30 20:40:42 +0800

    move to component folder

--- emart-web-frontend/src/app/app.module.ts
+++ emart-web-frontend/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmartMainNavigatorComponent } from './emart-main-navigator/emart-main-navigator.component';
-import { EmartMainFooterComponent } from './emart-main-footer/emart-main-footer.component';
-import { EmartCustomerIndexComponent } from './emart-customer-index/emart-customer-index.component';
-import { EmartLoginComponent } from './emart-login/emart-login.component';
-import { EmartCustomerSignupComponent } from './emart-customer-signup/emart-customer-signup.component';
-import { EmartSellerSignupComponent } from './emart-seller-signup/emart-seller-singup.component';
-import { EmartSellerSignupDetailComponent } from './emart-seller-signup-detail/emart-seller-signup-detail.component';
-import { EmartSearchResultComponent } from './emart-search-result/emart-search-result.component';
-import { EmartItemDetailComponent } from './emart-item-detail/emart-item-detail.component';
-import { EmartShoppingCartComponent } from './emart-shopping-cart/emart-shopping-cart.component';
-import { EmartPurchaseHistoryComponent } from './emart-purchase-history/emart-purchase-history.component';
-import { EmartSellerNavigatorComponent } from './emart-seller-navigator/emart-seller-navigator.component';
-import { EmartAddItemComponent } from './emart-add-item/emart-add-item.component';
-import { EmartViewStockComponent } from './emart-view-stock/emart-view-stock.component';
+import { EmartMainNavigatorComponent } from './component/emart-main-navigator/emart-main-navigator.component';
+import { EmartMainFooterComponent } from './component/emart-main-footer/emart-main-footer.component';
+import { EmartCustomerIndexComponent } from './component/emart-customer-index/emart-customer-index.component';
+import { EmartLoginComponent } from './component/emart-login/emart-login.component';
+import { EmartCustomerSignupComponent } from './component/emart-customer-signup/emart-customer-signup.component';
+import { EmartSellerSignupComponent } from './component/emart-seller-signup/emart-seller-singup.component';
+import { EmartSellerSignupDetailComponent } from './component/emart-seller-signup-detail/emart-seller-signup-detail.component';
+import { EmartSearchResultComponent } from './component/emart-search-result/emart-search-result.component';
+import { EmartItemDetailComponent } from './component/emart-item-detail/emart-item-detail.component';
+import { EmartShoppingCartComponent } from './component/emart-shopping-cart/emart-shopping-cart.component';
+import { EmartPurchaseHistoryComponent } from './component/emart-purchase-history/emart-purchase-history.component';
+import { EmartSellerNavigatorComponent } from './component/emart-seller-navigator/emart-seller-navigator.component';
+import { EmartAddItemComponent } from './component/emart-add-item/emart-add-item.component';
+import { EmartViewStockComponent } from './component/emart-view-stock/emart-view-stock.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
