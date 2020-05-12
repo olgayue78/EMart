a3d33857ea28bde7b779c90f0e56076ad9af8df3
olgayue78 <63052143+olgayue78@users.noreply.github.com>
2020-05-12 19:48:06 +0800

    signup seller

--- emart-web-frontend/src/app/component/emart-seller-signup-detail/emart-seller-signup-detail.component.ts
+++ emart-web-frontend/src/app/component/emart-seller-signup-detail/emart-seller-signup-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { ActivatedRoute } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-emart-seller-signup-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmartSellerSignupDetailComponent implements OnInit {
 
-  constructor() { }
-
+  public userDetail:User = new User();
+  public user:User = new User();
+  constructor(public activeRouter: ActivatedRoute,
+    private loginService:LoginService) { }
+  
   ngOnInit() {
+    this.user=JSON.parse(this.activeRouter.snapshot.queryParams["user"]);
   }
 
+  signup(){
+    debugger
+    this.userDetail.email = this.user.email;
+    this.userDetail.username = this.user.username;
+    this.userDetail.password = this.user.password;
+    this.userDetail.userRole = "2";
+    this.loginService.signUp(this.userDetail);
+  }
 }
--- emart-web-frontend/src/app/component/emart-seller-signup/emart-seller-singup.component.ts
+++ emart-web-frontend/src/app/component/emart-seller-signup/emart-seller-singup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-emart-seller-signup',
@@ -7,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./emart-seller-signup.component.css']
 })
 export class EmartSellerSignupComponent implements OnInit {
-
+  public user:User = new User();
   constructor(
     private route: ActivatedRoute,
     private router: Router) { }
@@ -16,6 +17,6 @@ export class EmartSellerSignupComponent implements OnInit {
   }
 
   nextClick(){
-    this.router.navigate(['/signup-seller-detail']);
+    this.router.navigate(['/signup-seller-detail'],{queryParams:{user:JSON.stringify(this.user)}});
   }
 }
