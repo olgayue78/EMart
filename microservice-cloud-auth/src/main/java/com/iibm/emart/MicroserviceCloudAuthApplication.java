bd4671c03b0cd6c448af70ef1078a8dba0985b09
olgayue78 <63052143+olgayue78@users.noreply.github.com>
2020-05-20 21:37:28 +0800

    Eureka

--- microservice-cloud-auth/src/main/java/com/iibm/emart/MicroserviceCloudAuthApplication.java
+++ microservice-cloud-auth/src/main/java/com/iibm/emart/MicroserviceCloudAuthApplication.java
@@ -2,8 +2,10 @@ package com.iibm.emart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviceCloudAuthApplication {
 
 	public static void main(String[] args) {
